e6fc66e4ce12fa3b72c91183184f047cb86976be
Hemerson Vianna <hemerson.lourenco@gmail.com>
2019-04-12 07:38:44 -0300

    feat: remove enzyme

--- internals/test/helper.js
+++ internals/test/helper.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, mount, render } from 'enzyme';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 Object.assign(global, {
   React,
   ReactDOM,
   ReactTestUtils,
-  shallow,
-  mount,
-  render,
 });
-
-global.shallowRender = jsx => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(jsx);
-  return renderer.getRenderOutput();
-};
